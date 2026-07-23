94c672002dd6b740e10f45b31f39b5e196bd86fe
Pranjal <pranjal.quantum@gmail.com>
2026-07-24 01:42:37 +0530

    installed django and created all needed files

---
+++ blogapp/models.py
@@ -0,0 +1,14 @@
+from django.db import models
+
+# Create your models here.
+class Article(models.Model):
+
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    likes = models.PositiveIntegerField(default=0)
+
+    def _str_(self):
+        return self.title
