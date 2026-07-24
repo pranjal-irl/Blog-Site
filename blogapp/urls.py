9be341ed8d388882180d3682da4f1aa914f34bb3
Pranjal <pranjal.quantum@gmail.com>
2026-07-24 23:41:30 +0530

    added live like and unlike button with htmx and django session

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('', views.home, name='home'),
     path('article/<slug:slug>/', views.article_detail, name='article_detail'),
+    path('article/<slug:slug>/like/', views.like_article, name='like_article'),
 ]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, get_object_or_404
+from django.http import HttpResponse
 from .models import Article
 
 # Create your views here.
@@ -8,4 +9,31 @@ def home(request):
 
 def article_detail(request, slug):
     article = get_object_or_404(Article, slug=slug)
-    return render(request, 'blogapp/article_detail.html', {article : article})
+    return render(request, 'blogapp/article_detail.html', {'article': article})
+
+def like_article(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+
+    if request.method == 'POST':
+        liked_articles = request.session.get('liked_articles', [])
+
+        if article.slug in liked_articles:
+            if article.likes > 0:
+                article.likes -= 1
+            liked_articles.remove(article.slug)
+            button_text = "Like"
+        else:
+            article.likes += 1
+            liked_articles.append(article.slug)
+            button_text = "Liked ❤️"
+
+        request.session['liked_articles'] = liked_articles
+        request.session.modified = True
+        article.save()
+
+
+        context = f"""
+        <span>Likes: {article.likes}</span>
+        <button hx-post="/article/{article.slug}/like/" hx-target="#like-section">{button_text}</button>
+        """
+        return HttpResponse(context)
