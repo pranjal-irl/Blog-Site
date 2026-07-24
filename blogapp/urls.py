57b83c64264ea2304e0d36d748c17005ea654eaf
Pranjal <pranjal.quantum@gmail.com>
2026-07-24 19:21:27 +0530

    made article details page

--- blogapp/urls.py
+++ blogapp/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
 from . import views
 
-urlpatterns: [
-    '', views.home, name='home'
-],
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('article/<slug:slug>/', views.article_detail, name='article_detail'),
+]
--- blogapp/views.py
+++ blogapp/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Article
 
 # Create your views here.
 def home(request):
-    articles = Article.subject.all()
+    articles = Article.objects.all()
     return render(request, 'blogapp/home.html', {'articles' : articles})
+
+def article_detail(request, slug):
+    article = get_object_or_404(Article, slug=slug)
+    return render(request, 'blogapp/article_detail.html', {article : article})
